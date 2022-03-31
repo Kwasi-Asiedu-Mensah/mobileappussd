265629b7f702c46f6890a4f04ae26ea80e1219a1
Kwasi-Asiedu-Mensah <70768599+Kwasi-Asiedu-Mensah@users.noreply.github.com>
2022-03-31 15:32:13 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $msisdn = $_POST['msisdn'];
+    $sequence_ID = $_POST['sequenceID'];
+    $data = $_POST['data'];
+    $type = 1;
+
+  
+    $mereply = "Hello Kwasi Asiedu-Mensah";
+    echo $mereply.'|'.$msisdn.'|'.$sequence_id.'|'.$type;
+ 
+
+
+?>
+
